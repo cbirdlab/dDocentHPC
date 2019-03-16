2b8f5dd7343dbbeea7d09c3ac6f5c5b449b17905
Chris Bird <cbird808@gmail.com>
2019-03-16 08:07:41 -0500

    added scripts

--- scripts/runFASTQC.bash
+++ scripts/runFASTQC.bash
@@ -4,9 +4,6 @@
 #SBATCH --time=96:00:00
 #SBATCH -p normal
 #SBATCH --nodes=1
-#SBATCH --mail-user=cbird@tamucc.edu
-#SBATCH --mail-type=begin  # email me when the job starts
-#SBATCH --mail-type=end    # email me when the job finish
 
 module load fastqc
 module load parallel
---
+++ scripts/stdir3.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#run this in your new project directory using the following command:
+#bash stdir3.bash
+#or
+#. stdir3.bash
+
+mkdir mkREF
+mkdir mkREF/FASTQC
+mkdir fastq
+mkdir FASTQC
+mkdir mkBAM
+mkdir mkBAM/FASTQC
+mkdir filtering
